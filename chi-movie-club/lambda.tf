49f7c0153dc26465a07ccfe532c1e427fca1ffad
Clifford <greenclifford30@gmail.com>
2025-06-06 09:08:46 -0500

    Update the correct lambda target.

--- chi-movie-club/lambda.tf
+++ chi-movie-club/lambda.tf
@@ -30,7 +30,7 @@ resource "aws_lambda_function" "movie_scraper" {
 
 resource "aws_lambda_event_source_mapping" "admin_selection_sqs_trigger" {
   event_source_arn = aws_sqs_queue.admin_selection.arn
-  function_name    = aws_lambda_function.admin_selection.arn
+  function_name    = aws_lambda_function.movie_scraper.arn
   batch_size       = 1
   enabled          = true
 }
