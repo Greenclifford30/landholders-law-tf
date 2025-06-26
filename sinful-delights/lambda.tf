bfc8261c25e76049d0c5679b97fcf614b3062d91
Clifford <greenclifford30@gmail.com>
2025-06-26 13:36:09 -0500

    Update module

--- sinful-delights/lambda.tf
+++ sinful-delights/lambda.tf
@@ -1,5 +1,5 @@
 module "create_menu_lambda" {
-  source     = "./modules/python_lambda"
+  source     = "./modules/python_module"
   function_name = "${var.app_underscore}_create_menu"
   role_arn      = aws_iam_role.sinflul_delights_lambda_role.arn
   environment_variables = {
