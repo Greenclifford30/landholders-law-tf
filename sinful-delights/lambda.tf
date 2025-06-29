cfbbee54403080b09684e3456090329af46a38b2
Clifford <greenclifford30@gmail.com>
2025-06-29 12:38:45 -0500

    Update the lambda name.

--- sinful-delights/lambda.tf
+++ sinful-delights/lambda.tf
@@ -1,6 +1,6 @@
 module "create_menu_lambda" {
   source     = "./modules/python_module"
-  function_name = "${var.app}-create-menu"
+  function_name = "${var.app}-create-menu-lambda"
   role_arn      = aws_iam_role.sinflul_delights_lambda_role.arn
   environment_variables = {
     TABLE_NAME = aws_dynamodb_table.sinful_delights.name
