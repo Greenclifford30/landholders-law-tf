059b380d196117c02ef65d179f639d9b73e570d8
Clifford <greenclifford30@gmail.com>
2025-07-08 18:39:08 -0500

    update endpoints.

--- stricklin/apig.tf
+++ stricklin/apig.tf
@@ -24,15 +24,15 @@ resource "aws_api_gateway_resource" "checkin" {
   path_part   = "checkin"
 }
 
-# module "checkin_post" {
-#   source      = "./modules/apigateway_method"
-#   api_id      = aws_api_gateway_rest_api.stricklin_api.id
-#   resource_id = aws_api_gateway_resource.checkin.id
-#   http_method = "POST"
-#   lambda_arn  = module.checkin_post.lambda_function_arn
-#   lambda_invoke_arn = module.checkin_post.lambda_invoke_arn
-#   apig_gateway_source_arn = aws_api_gateway_rest_api.stricklin_api.execution_arn
-# }
+module "checkin_post" {
+  source      = "./modules/apigateway_method"
+  api_id      = aws_api_gateway_rest_api.stricklin_api.id
+  resource_id = aws_api_gateway_resource.checkin.id
+  http_method = "POST"
+  lambda_arn  = module.checkin_post.lambda_function_arn
+  lambda_invoke_arn = module.checkin_post.lambda_invoke_arn
+  apig_gateway_source_arn = aws_api_gateway_rest_api.stricklin_api.execution_arn
+}
 
 resource "aws_api_gateway_resource" "dashboard" {
   rest_api_id = aws_api_gateway_rest_api.stricklin_api.id
@@ -41,15 +41,15 @@ resource "aws_api_gateway_resource" "dashboard" {
 }
 
 
-# module "get_dashboard_post" {
-#   source      = "./modules/apigateway_method"
-#   api_id      = aws_api_gateway_rest_api.stricklin_api.id
-#   resource_id = aws_api_gateway_resource.dashboard.id
-#   http_method = "GET"
-#   lambda_arn  = module.get_dashboard_lambda.lambda_function_arn
-#   lambda_invoke_arn = module.get_dashboard_lambda.lambda_invoke_arn
-#   apig_gateway_source_arn = aws_api_gateway_rest_api.stricklin_api.execution_arn
-# }
+module "get_dashboard_post" {
+  source      = "./modules/apigateway_method"
+  api_id      = aws_api_gateway_rest_api.stricklin_api.id
+  resource_id = aws_api_gateway_resource.dashboard.id
+  http_method = "GET"
+  lambda_arn  = module.get_dashboard_lambda.lambda_function_arn
+  lambda_invoke_arn = module.get_dashboard_lambda.lambda_invoke_arn
+  apig_gateway_source_arn = aws_api_gateway_rest_api.stricklin_api.execution_arn
+}
 #########################################
 # 6) Create a Deployment and a Stage
 #########################################
