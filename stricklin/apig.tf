e85c9c735f1fdfcae882df2b122b4dfbb74a103b
Clifford <greenclifford30@gmail.com>
2025-07-08 18:41:11 -0500

    Update module reference.

--- stricklin/apig.tf
+++ stricklin/apig.tf
@@ -29,8 +29,8 @@ module "checkin_post" {
   api_id      = aws_api_gateway_rest_api.stricklin_api.id
   resource_id = aws_api_gateway_resource.checkin.id
   http_method = "POST"
-  lambda_arn  = module.checkin_post.lambda_function_arn
-  lambda_invoke_arn = module.checkin_post.lambda_invoke_arn
+  lambda_arn  = module.post_checkin_lambda.lambda_function_arn
+  lambda_invoke_arn = module.post_checkin_lambda.lambda_invoke_arn
   apig_gateway_source_arn = aws_api_gateway_rest_api.stricklin_api.execution_arn
 }
 
