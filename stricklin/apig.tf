f724f9b277a675b7ac069dd05f3c1e9ad0bee9ea
Clifford <greenclifford30@gmail.com>
2025-07-08 18:32:03 -0500

    Update API

--- stricklin/apig.tf
+++ stricklin/apig.tf
@@ -29,8 +29,8 @@ module "checkin_post" {
   api_id      = aws_api_gateway_rest_api.stricklin_api.id
   resource_id = aws_api_gateway_resource.checkin.id
   http_method = "POST"
-  lambda_arn  = module.create_menu_lambda.lambda_function_arn
-  lambda_invoke_arn = module.create_menu_lambda.lambda_invoke_arn
+  lambda_arn  = module.checkin_post.lambda_function_arn
+  lambda_invoke_arn = module.checkin_post.lambda_invoke_arn
   apig_gateway_source_arn = aws_api_gateway_rest_api.stricklin_api.execution_arn
 }
 
@@ -46,8 +46,8 @@ module "get_dashboard_post" {
   api_id      = aws_api_gateway_rest_api.stricklin_api.id
   resource_id = aws_api_gateway_resource.dashboard.id
   http_method = "GET"
-  lambda_arn  = module.create_menu_lambda.lambda_function_arn
-  lambda_invoke_arn = module.create_menu_lambda.lambda_invoke_arn
+  lambda_arn  = module.get_dashboard_lambda.lambda_function_arn
+  lambda_invoke_arn = module.get_dashboard_lambda.lambda_invoke_arn
   apig_gateway_source_arn = aws_api_gateway_rest_api.stricklin_api.execution_arn
 }
 #########################################
