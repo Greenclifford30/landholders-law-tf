04bdb9225bf0fb3b777181c7d24f9edc8854f73c
Clifford <greenclifford30@gmail.com>
2025-07-19 18:12:02 -0500

    Update api gateway

--- sinful-delights/apig.tf
+++ sinful-delights/apig.tf
@@ -69,15 +69,6 @@ resource "aws_api_gateway_resource" "admin_inventory" {
   path_part   = "inventory"
 }
 
-#########################################
-# 2) Create the "/admin/menu" Resource
-#########################################
-resource "aws_api_gateway_resource" "admin_menu" {
-  rest_api_id = aws_api_gateway_rest_api.sinful_delights_api.id
-  parent_id   = aws_api_gateway_resource.admin.id
-  path_part   = "menu"
-}
-
 # GET /v1/menu/today
 module "get_menu_today" {
   source      = "./modules/apigateway_method"
