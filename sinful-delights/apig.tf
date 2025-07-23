7050b9d6cc75c84953ea816c4f708f724464ea5b
Clifford <greenclifford30@gmail.com>
2025-07-23 15:30:13 -0500

    New methods.

--- sinful-delights/apig.tf
+++ sinful-delights/apig.tf
@@ -14,15 +14,16 @@ resource "aws_api_gateway_resource" "v1" {
 }
 
 # Child resources
-resource "aws_api_gateway_resource" "menu_today" {
+
+resource "aws_api_gateway_resource" "menu" {
   rest_api_id = aws_api_gateway_rest_api.sinful_delights_api.id
   parent_id   = aws_api_gateway_resource.v1.id
   path_part   = "menu"
 }
 
-resource "aws_api_gateway_resource" "menu_today_today" {
+resource "aws_api_gateway_resource" "menu_today" {
   rest_api_id = aws_api_gateway_rest_api.sinful_delights_api.id
-  parent_id   = aws_api_gateway_resource.menu_today.id
+  parent_id   = aws_api_gateway_resource.menu.id
   path_part   = "today"
 }
 
@@ -63,17 +64,64 @@ resource "aws_api_gateway_resource" "admin_menu" {
   path_part   = "menu"
 }
 
+resource "aws_api_gateway_resource" "admin_menus" {
+  rest_api_id = aws_api_gateway_rest_api.sinful_delights_api.id
+  parent_id   = aws_api_gateway_resource.admin.id
+  path_part   = "menus"
+}
+
+resource "aws_api_gateway_resource" "admin_menu_id" {
+  rest_api_id = aws_api_gateway_rest_api.sinful_delights_api.id
+  parent_id   = aws_api_gateway_resource.admin_menu.id
+  path_part   = "{menuId}"
+}
+
+resource "aws_api_gateway_resource" "admin_menu_template" {
+  rest_api_id = aws_api_gateway_rest_api.sinful_delights_api.id
+  parent_id   = aws_api_gateway_resource.admin.id
+  path_part   = "menu-template"
+}
+
+resource "aws_api_gateway_resource" "admin_menu_templates" {
+  rest_api_id = aws_api_gateway_rest_api.sinful_delights_api.id
+  parent_id   = aws_api_gateway_resource.admin.id
+  path_part   = "menu-templates"
+}
+
+resource "aws_api_gateway_resource" "admin_menu_template_id" {
+  rest_api_id = aws_api_gateway_rest_api.sinful_delights_api.id
+  parent_id   = aws_api_gateway_resource.admin_menu_template.id
+  path_part   = "{templateId}"
+}
+
+resource "aws_api_gateway_resource" "admin_menu_import" {
+  rest_api_id = aws_api_gateway_rest_api.sinful_delights_api.id
+  parent_id   = aws_api_gateway_resource.admin.id
+  path_part   = "menu"
+}
+
 resource "aws_api_gateway_resource" "admin_inventory" {
   rest_api_id = aws_api_gateway_rest_api.sinful_delights_api.id
   parent_id   = aws_api_gateway_resource.admin.id
   path_part   = "inventory"
 }
 
+# GET /v1/menu
+module "get_menu_today" {
+  source      = "./modules/apigateway_method"
+  api_id      = aws_api_gateway_rest_api.sinful_delights_api.id
+  resource_id = aws_api_gateway_resource.menu.id
+  http_method = "GET"
+  lambda_arn  = module.get_menu_today_lambda.lambda_function_arn
+  lambda_invoke_arn = module.get_menu_today_lambda.lambda_invoke_arn
+  apig_gateway_source_arn = aws_api_gateway_rest_api.sinful_delights_api.execution_arn
+}
+
 # GET /v1/menu/today
 module "get_menu_today" {
   source      = "./modules/apigateway_method"
   api_id      = aws_api_gateway_rest_api.sinful_delights_api.id
-  resource_id = aws_api_gateway_resource.menu_today_today.id
+  resource_id = aws_api_gateway_resource.menu_today.id
   http_method = "GET"
   lambda_arn  = module.get_menu_today_lambda.lambda_function_arn
   lambda_invoke_arn = module.get_menu_today_lambda.lambda_invoke_arn
@@ -135,6 +183,54 @@ module "get_admin_analytics" {
   apig_gateway_source_arn = aws_api_gateway_rest_api.sinful_delights_api.execution_arn
 }
 
+# GET /v1/admin/menu
+module "get_admin_menu" {
+  source      = "./modules/apigateway_method"
+  api_id      = aws_api_gateway_rest_api.sinful_delights_api.id
+  resource_id = aws_api_gateway_resource.admin_menu.id
+  http_method = "GET"
+  lambda_arn  = module.post_admin_menu_lambda.lambda_function_arn
+  lambda_invoke_arn = module.post_admin_menu_lambda.lambda_invoke_arn
+  apig_gateway_source_arn = aws_api_gateway_rest_api.sinful_delights_api.execution_arn
+}
+
+# GET /v1/admin/menus
+module "get_admin_menus" {
+  source      = "./modules/apigateway_method"
+  api_id      = aws_api_gateway_rest_api.sinful_delights_api.id
+  resource_id = aws_api_gateway_resource.admin_menus.id
+  http_method = "GET"
+  lambda_arn  = module.post_admin_menu_lambda.lambda_function_arn
+  lambda_invoke_arn = module.post_admin_menu_lambda.lambda_invoke_arn
+  apig_gateway_source_arn = aws_api_gateway_rest_api.sinful_delights_api.execution_arn
+}
+
+# GET /v1/admin/menu/{menuId}
+module "get_admin_menu_by_id" {
+  source      = "./modules/apigateway_method"
+  api_id      = aws_api_gateway_rest_api.sinful_delights_api.id
+  resource_id = aws_api_gateway_resource.admin_menu_id.id
+  http_method = "GET"
+  lambda_arn  = module.get_admin_menu_by_id_lambda.lambda_function_arn
+  lambda_invoke_arn = module.get_admin_menu_by_id_lambda.lambda_invoke_arn
+  apig_gateway_source_arn = aws_api_gateway_rest_api.sinful_delights_api.execution_arn
+  expect_uri_parameter = true
+  uri_param = "{menuId}"
+}
+
+# DELETE /v1/admin/menu/{menuId}
+module "delete_admin_menu_by_id" {
+  source      = "./modules/apigateway_method"
+  api_id      = aws_api_gateway_rest_api.sinful_delights_api.id
+  resource_id = aws_api_gateway_resource.admin_menu_id.id
+  http_method = "DELETE"
+  lambda_arn  = module.delete_admin_menu_by_id_lambda.lambda_function_arn
+  lambda_invoke_arn = module.delete_admin_menu_by_id_lambda.lambda_invoke_arn
+  apig_gateway_source_arn = aws_api_gateway_rest_api.sinful_delights_api.execution_arn
+  expect_uri_parameter = true
+  uri_param = "{menuId}"
+}
+
 # POST /v1/admin/menu
 module "post_admin_menu" {
   source      = "./modules/apigateway_method"
@@ -144,6 +240,67 @@ module "post_admin_menu" {
   lambda_arn  = module.post_admin_menu_lambda.lambda_function_arn
   lambda_invoke_arn = module.post_admin_menu_lambda.lambda_invoke_arn
   apig_gateway_source_arn = aws_api_gateway_rest_api.sinful_delights_api.execution_arn
+}
+
+# GET /v1/admin/menu-templates
+module "get_admin_menu_template" {
+  source      = "./modules/apigateway_method"
+  api_id      = aws_api_gateway_rest_api.sinful_delights_api.id
+  resource_id = aws_api_gateway_resource.admin_menu_templates.id
+  http_method = "GET"
+  lambda_arn  = module.get_admin_menu_template_lambda.lambda_function_arn
+  lambda_invoke_arn = module.get_admin_menu_template_lambda.lambda_invoke_arn
+  apig_gateway_source_arn = aws_api_gateway_rest_api.sinful_delights_api.execution_arn
+}
+
+# POST /v1/admin/menu-template
+module "post_admin_menu_template" {
+  source      = "./modules/apigateway_method"
+  api_id      = aws_api_gateway_rest_api.sinful_delights_api.id
+  resource_id = aws_api_gateway_resource.admin_menu_template.id
+  http_method = "POST"
+  lambda_arn  = module.post_admin_menu_template_lambda.lambda_function_arn
+  lambda_invoke_arn = module.post_admin_menu_template_lambda.lambda_invoke_arn
+  apig_gateway_source_arn = aws_api_gateway_rest_api.sinful_delights_api.execution_arn
+}
+
+# GET /v1/admin/menu-templates/{templateId}
+module "get_admin_menu_template_by_id" {
+  source      = "./modules/apigateway_method"
+  api_id      = aws_api_gateway_rest_api.sinful_delights_api.id
+  resource_id = aws_api_gateway_resource.admin_menu_template_id.id
+  http_method = "PUT"
+  lambda_arn  = module.get_admin_menu_template_by_id_lambda.lambda_function_arn
+  lambda_invoke_arn = module.get_admin_menu_template_by_id_lambda.lambda_invoke_arn
+  apig_gateway_source_arn = aws_api_gateway_rest_api.sinful_delights_api.execution_arn
+  expect_uri_parameter = true
+  uri_param = "{templateId}"
+}
+
+# PUT /v1/admin/menu-templates/{templateId}
+module "put_admin_menu_template" {
+  source      = "./modules/apigateway_method"
+  api_id      = aws_api_gateway_rest_api.sinful_delights_api.id
+  resource_id = aws_api_gateway_resource.admin_menu_template_id.id
+  http_method = "PUT"
+  lambda_arn  = module.put_admin_menu_template_lambda.lambda_function_arn
+  lambda_invoke_arn = module.put_admin_menu_template_lambda.lambda_invoke_arn
+  apig_gateway_source_arn = aws_api_gateway_rest_api.sinful_delights_api.execution_arn
+  expect_uri_parameter = true
+  uri_param = "{templateId}"
+}
+
+# DELETE /v1/admin/menu-templates/{templateId}
+module "delete_admin_menu_template" {
+  source      = "./modules/apigateway_method"
+  api_id      = aws_api_gateway_rest_api.sinful_delights_api.id
+  resource_id = aws_api_gateway_resource.admin_menu_template_id.id
+  http_method = "DELETE"
+  lambda_arn  = module.delete_admin_menu_template_lambda.lambda_function_arn
+  lambda_invoke_arn = module.delete_admin_menu_template_lambda.lambda_invoke_arn
+  apig_gateway_source_arn = aws_api_gateway_rest_api.sinful_delights_api.execution_arn
+  expect_uri_parameter = true
+  uri_param = "{templateId}"
 }
 
 # POST /v1/admin/inventory
--- sinful-delights/lambda.tf
+++ sinful-delights/lambda.tf
@@ -51,9 +51,90 @@ module "get_admin_analytics_lambda" {
   }
 }
 
+module "get_admin_menu_lambda" {
+  source            = "./modules/python_module"
+  function_name     = "${var.app}-get-admin-menu-lambda"
+  role_arn          = aws_iam_role.sinflul_delights_lambda_role.arn
+  environment_variables = {
+    TABLE_NAME = aws_dynamodb_table.menu.arn
+  }
+}
+
+module "get_admin_menus_lambda" {
+  source            = "./modules/python_module"
+  function_name     = "${var.app}-get-admin-menus-lambda"
+  role_arn          = aws_iam_role.sinflul_delights_lambda_role.arn
+  environment_variables = {
+    TABLE_NAME = aws_dynamodb_table.menu.arn
+  }
+}
+
+module "get_admin_menu_by_id_lambda" {
+  source            = "./modules/python_module"
+  function_name     = "${var.app}-get-admin-menu-by-id-lambda"
+  role_arn          = aws_iam_role.sinflul_delights_lambda_role.arn
+  environment_variables = {
+    TABLE_NAME = aws_dynamodb_table.menu.arn
+  }
+}
+
+module "delete_admin_menu_by_id_lambda" {
+  source            = "./modules/python_module"
+  function_name     = "${var.app}-delete-admin-menu-by-id-lambda"
+  role_arn          = aws_iam_role.sinflul_delights_lambda_role.arn
+  environment_variables = {
+    TABLE_NAME = aws_dynamodb_table.menu.arn
+  }
+}
+
 module "post_admin_menu_lambda" {
   source            = "./modules/python_module"
   function_name     = "${var.app}-post-admin-menu-lambda"
+  role_arn          = aws_iam_role.sinflul_delights_lambda_role.arn
+  environment_variables = {
+    TABLE_NAME = aws_dynamodb_table.menu.arn
+  }
+}
+
+module "post_admin_menu_template_lambda" {
+  source            = "./modules/python_module"
+  function_name     = "${var.app}-post-admin-menu-template-lambda"
+  role_arn          = aws_iam_role.sinflul_delights_lambda_role.arn
+  environment_variables = {
+    TABLE_NAME = aws_dynamodb_table.menu.arn
+  }
+}
+
+module "get_admin_menu_template_lambda" {
+  source            = "./modules/python_module"
+  function_name     = "${var.app}-get-admin-menu-template-lambda"
+  role_arn          = aws_iam_role.sinflul_delights_lambda_role.arn
+  environment_variables = {
+    TABLE_NAME = aws_dynamodb_table.menu.arn
+  }
+}
+
+module "get_admin_menu_template_by_id_lambda" {
+  source            = "./modules/python_module"
+  function_name     = "${var.app}-get-admin-menu-template-by-id-lambda"
+  role_arn          = aws_iam_role.sinflul_delights_lambda_role.arn
+  environment_variables = {
+    TABLE_NAME = aws_dynamodb_table.menu.arn
+  }
+}
+
+module "put_admin_menu_template_lambda" {
+  source            = "./modules/python_module"
+  function_name     = "${var.app}-put-admin-menu-template-lambda"
+  role_arn          = aws_iam_role.sinflul_delights_lambda_role.arn
+  environment_variables = {
+    TABLE_NAME = aws_dynamodb_table.menu.arn
+  }
+}
+
+module "delete_admin_menu_template_lambda" {
+  source            = "./modules/python_module"
+  function_name     = "${var.app}-delete-admin-menu-template-lambda"
   role_arn          = aws_iam_role.sinflul_delights_lambda_role.arn
   environment_variables = {
     TABLE_NAME = aws_dynamodb_table.menu.arn
