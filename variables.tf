0732f822ee9615478de2ca5c8c78719d5f7e6f70
Clifford <greenclifford30@gmail.com>
2026-09-24 13:04:25 -0500

    Create updated variables for module.

--- variables.tf
+++ variables.tf
@@ -54,11 +54,31 @@ variable "chimovieclub_cognito_google_client_secret" {
 variable "chimovieclub_cognito_callback_urls" {
   type        = list(string)
   description = "Allowed Chi Movie Club Cognito OAuth callback URLs."
-  default     = ["http://localhost:3000/auth/callback"]
+  default     = ["https://chicagomovieclub.app/callback", "http://localhost:3000/auth/callback"]
 }
 
 variable "chimovieclub_cognito_logout_urls" {
   type        = list(string)
   description = "Allowed Chi Movie Club Cognito OAuth sign-out redirect URLs."
-  default     = ["http://localhost:3000/sign-in"]
+  default     = ["https://chicagomovieclub.app/sign-in", "http://localhost:3000/sign-in"]
+}
+
+
+variable "movie_club_web_push_vapid_public_key" {
+  type        = string
+  description = "Base64url VAPID public key used by the Movie Club web client to create browser push subscriptions."
+  default     = ""
+}
+
+variable "movie_club_web_push_vapid_private_key" {
+  type        = string
+  description = "Base64url VAPID private key used only by the notification worker to sign browser push messages."
+  sensitive   = true
+  default     = ""
+}
+
+variable "movie_club_web_push_vapid_claims_email" {
+  type        = string
+  description = "Contact email used in the VAPID subject claim, for example mailto:you@chicagomovieclub.app"
+  default     = "mailto:you@chicagomovieclub.app"
 }
